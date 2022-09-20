d771773fcd0790691b38ac87f011b7017634da0e
Li Guangda <1500544774@qq.com>
2022-09-20 15:04:37 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -h mariadb -u root -p123456 < /var/www/html/create.sql
-mysql -h mariadb -u root -p123456 qqfarm < /var/www/html/qqfarm.sql
+mysql -h mariadb -u root -p123 < /var/www/html/create.sql
+mysql -h mariadb -u root -p123 qqfarm < /var/www/html/qqfarm.sql
