975bd0add0a2acf17115bfb79745e9e1a5a3fbff
Li Guangda <1500544774@qq.com>
2022-09-21 15:28:54 +0800

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
 
-echo -e "更新源,升级依赖"
+if_failed_exit() {
+  if [ $? -ne 0 ]; then
+    echo "Error"
+  fi
+  exit $?
+}
+
+echo -e "\n更新源,升级依赖"
 apt-get -y update && apt -y upgrade
 
-echo -e "安装常用命令"
+if_failed_exit
+
+echo -e "\n安装常用命令"
 apt-get install -y git unzip 
 
-echo -e "安装Docker和Docker-Compose"
+if_failed_exit
+
+echo -e "\n安装Docker和Docker-Compose"
 apt-get install -y docker.io docker-compose
 
-echo -e "拉取mariadb镜像"
+if_failed_exit
+
+echo -e "\n拉取mariadb镜像"
 docker pull mariadb:10.9.2
 
-echo -e "构建php-apache-mysql镜像"
+if_failed_exit
+
+echo -e "\n构建php-apache-mysql镜像"
 docker build -t php-apache-mysql .
 
-echo -e "构建映射目录"
+if_failed_exit
+
+echo -e "\n构建映射目录"
 mkdir -vp /data/php-apache/html /data/mysql/data
 cp -v php.ini create.sql step2.sh /data/php-apache/html
 
-echo -e "解压discuz论坛文件"
+if_failed_exit
+
+echo -e "\n解压discuz论坛文件"
 unzip farm-ucenter1.5.zip
 mv upload/* /data/php-apache/html
 chmod 777 -R /data/php-apache/html
 
-echo -e "运行docker-compose"
+if_failed_exit
+
+echo -e "\n运行docker-compose"
 docker-compose up -d
 
-echo -e "导入数据表"
+if_failed_exit
+
+echo -e "\n等待5秒，导入数据表"
 sleep 5
 docker exec php /var/www/html/step2.sh
 
-echo -e "准备工作完成！"
+if_failed_exit
+
+echo -e "\n准备工作完成！"
